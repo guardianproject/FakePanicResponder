75a49c8e201700d526b2eece2067232467e2e398
Hans-Christoph Steiner <hans@eds.org>
2015-10-14 20:36:18 +0200

    add two config items: Show Toast and Uninstall This App
    makes the example a lot more useful if there are actual actions.

--- src/info/guardianproject/fakepanicreceiver/PanicConfigActivity.java
+++ src/info/guardianproject/fakepanicreceiver/PanicConfigActivity.java
@@ -3,17 +3,23 @@ package info.guardianproject.fakepanicreceiver;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import info.guardianproject.panic.PanicReceiver;
 
 public class PanicConfigActivity extends Activity {
+
+    public static final String PREF_SHOW_TOAST = "pref_show_toast";
+    public static final String PREF_UNINSTALL_THIS_APP = "pref_uninstall_this_app";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +30,22 @@ public class PanicConfigActivity extends Activity {
 
         setContentView(R.layout.activity_panic_config);
 
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
         final TextView contentView = (TextView) findViewById(R.id.fullscreen_content);
         final Intent intent = getIntent();
         final String action = intent.getAction();
         if (TextUtils.isEmpty(action)) {
             // TODO this app is initiating the connection to the Panic Button
-            contentView.setText("I'm the decider!");
+            contentView.setText("App-local config");
         } else {
             contentView.setText(action);
         }
+
+        final CheckBox showToast = (CheckBox) findViewById(R.id.show_toast);
+        showToast.setChecked(prefs.getBoolean(PREF_SHOW_TOAST, true));
+        final CheckBox uninstallThisApp = (CheckBox) findViewById(R.id.uninstall_this_app);
+        uninstallThisApp.setChecked(prefs.getBoolean(PREF_UNINSTALL_THIS_APP, false));
 
         final Button cancelButton = (Button) findViewById(R.id.cancel_button);
         cancelButton.setOnClickListener(new OnClickListener() {
@@ -49,12 +62,16 @@ public class PanicConfigActivity extends Activity {
 
             @Override
             public void onClick(View v) {
+                prefs.edit()
+                        .putBoolean(PREF_SHOW_TOAST, showToast.isChecked())
+                        .putBoolean(PREF_UNINSTALL_THIS_APP, uninstallThisApp.isChecked())
+                        .apply();
+
                 Activity activity = PanicConfigActivity.this;
                 PanicReceiver.setTriggerPackageName(activity);
                 activity.setResult(Activity.RESULT_OK, intent);
                 finish();
             }
         });
-
     }
 }
--- src/info/guardianproject/fakepanicreceiver/PanicTriggerActivity.java
+++ src/info/guardianproject/fakepanicreceiver/PanicTriggerActivity.java
@@ -2,7 +2,11 @@
 package info.guardianproject.fakepanicreceiver;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -11,9 +15,18 @@ public class PanicTriggerActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String msg = "FakePanicReceiver got a panic trigger!";
-        Log.i("PanicTriggerActivity", msg);
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getBoolean(PanicConfigActivity.PREF_SHOW_TOAST, true)) {
+            String msg = "FakePanicReceiver got a panic trigger!";
+            Log.i("PanicTriggerActivity", msg);
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        }
+        if (prefs.getBoolean(PanicConfigActivity.PREF_UNINSTALL_THIS_APP, false)) {
+            Intent intent = new Intent(Intent.ACTION_DELETE);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+        }
         finish();
     }
 }
