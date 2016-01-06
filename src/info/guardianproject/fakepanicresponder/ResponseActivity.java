f7633c64639736355286b9a5d8af044bb3d9f0dd
Hans-Christoph Steiner <hans@eds.org>
2016-01-06 17:06:21 +0100

    update to PanicKit v0.4 responder flow
    This uses the methods from PanicResponder to determine whether it should
    use the default response, or the connected/configured response.

--- src/info/guardianproject/fakepanicresponder/ResponseActivity.java
+++ src/info/guardianproject/fakepanicresponder/ResponseActivity.java
@@ -10,25 +10,40 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
+import info.guardianproject.panic.PanicResponder;
+
 public class ResponseActivity extends Activity {
+    public static final String TAG = "ResponseActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String msg = "FakePanicResponder got a panic trigger!";
         Log.i(TAG, msg);
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
 
-        if (prefs.getBoolean(PanicConfigActivity.PREF_LOCK_AND_EXIT, true)) {
-            ExitActivity.exitAndRemoveFromRecentApps(this);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (PanicResponder.receivedTriggerFromConnectedApp(this)) {
+            if (prefs.getBoolean(PanicConfigActivity.PREF_UNINSTALL_THIS_APP, false)) {
+                Log.i(TAG, PanicConfigActivity.PREF_UNINSTALL_THIS_APP);
+                PanicResponder.deleteAllAppData(this);
+                Intent uninstall = new Intent(Intent.ACTION_DELETE);
+                uninstall.setData(Uri.parse("package:" + getPackageName()));
+                startActivity(uninstall);
+                ExitActivity.exitAndRemoveFromRecentApps(this);
+            } else if (prefs.getBoolean(PanicConfigActivity.PREF_LOCK_AND_EXIT, true)) {
+                Log.i(TAG, PanicConfigActivity.PREF_LOCK_AND_EXIT);
+                ExitActivity.exitAndRemoveFromRecentApps(this);
+            }
+            // add other responses here, paying attention to if/else order
+        } else if (PanicResponder.shouldUseDefaultResponseToTrigger(this)) {
+            if (prefs.getBoolean(PanicConfigActivity.PREF_LOCK_AND_EXIT, true)) {
+                Log.i(TAG, PanicConfigActivity.PREF_LOCK_AND_EXIT);
+                ExitActivity.exitAndRemoveFromRecentApps(this);
+            }
         }
-        if (prefs.getBoolean(PanicConfigActivity.PREF_UNINSTALL_THIS_APP, false)) {
-            Intent intent = new Intent(Intent.ACTION_DELETE);
-            intent.setData(Uri.parse("package:" + getPackageName()));
-            startActivity(intent);
-        }
+
         finish();
     }
 }
